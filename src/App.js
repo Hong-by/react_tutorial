94f86534f2a11dd90a0c04764b3cf8d63a9f3bea
Bye-Hong <hby033@naver.com>
2021-08-25 14:10:48 +0900

    bye-hong
    setting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
 
+import React from "react";
+
+const arr=[1,2,3]
+  arr.map((a) => {
+    return a;
+  })
+//내가 만든 컴포넌트 > App.js > index.js > index.html
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
     </div>
   );
 }
